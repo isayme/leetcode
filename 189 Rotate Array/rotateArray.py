bfdf0b10a95d4cf51bc1b0409e8b18e0c63ee710
isayme <isaymeorg@gmail.com>
2015-05-06 17:43:19 +0800

    189 Rotate Array

---
+++ 189 Rotate Array/rotateArray.py
@@ -0,0 +1,8 @@
+class Solution:
+    # @param {integer[]} nums
+    # @param {integer} k
+    # @return {void} Do not return anything, modify nums in-place instead.
+    def rotate(self, nums, k):
+        while k:
+            nums.insert(0, nums.pop())
+            k -= 1
